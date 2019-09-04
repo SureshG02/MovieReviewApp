e430a49b59fd8efdd6be9d94e8607c24b18843f7
Gupta Suresh <suresh.gupta@digia.com>
2019-09-04 16:00:35 +0300

    Removed threading code.

--- movie-review-backend/src/main/java/com/gofore/movie/review/MovieReviewApplication.java
+++ movie-review-backend/src/main/java/com/gofore/movie/review/MovieReviewApplication.java
@@ -1,13 +1,10 @@
 package com.gofore.movie.review;
 
-import java.util.concurrent.Executor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 @ComponentScan("com.gofore.movie.review")
@@ -22,16 +19,5 @@ public class MovieReviewApplication {
 	@Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
-    }
-	
-    @Bean
-    public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(4);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("MovieReview-");
-        executor.initialize();
-        return executor;
     }
 }
--- movie-review-backend/src/main/java/com/gofore/movie/review/client/RestClient.java
+++ movie-review-backend/src/main/java/com/gofore/movie/review/client/RestClient.java
@@ -3,13 +3,10 @@ package com.gofore.movie.review.client;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,7 +22,7 @@ import com.gofore.movie.review.model.Search;
 public class RestClient {
 
 	private static final Logger logger = LoggerFactory.getLogger(RestClient.class);
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
 
@@ -34,21 +31,23 @@ public class RestClient {
 
 	@Value("${omdb.apikey}")
 	private String omdbUrlApikey;
-	
+
 	@Value("${nyt.url}")
 	private String nytUrl;
-	
+
 	@Value("${nyt.apikey}")
 	private String nytUrlApikey;
-	
+
 	/**
-	 * Fetches movie review data from OMDB API and NYT API for each movie title and returns MovieResponse.
+	 * Fetches movie review data from OMDB API and NYT API for each movie title and
+	 * returns MovieResponse.
+	 * 
 	 * @param movieSearch
 	 * @param page
 	 * @return MovieResponse
 	 * @throws InterruptedException
 	 */
-	public MovieResponse generateMovieSummary(String movieSearch, Integer ...page) throws InterruptedException {
+	public MovieResponse generateMovieSummary(String movieSearch, Integer... page) throws InterruptedException {
 		List<MovieSummary> summaryList = new ArrayList<MovieSummary>();
 		MovieResponse response = new MovieResponse();
 		String omdbUrlByS = "";
@@ -58,7 +57,7 @@ public class RestClient {
 			} else {
 				omdbUrlByS = omdbUrl + "/?s=" + movieSearch + "&apikey=" + omdbUrlApikey;
 			}
-			
+
 			MovieReviewOmdbByS movieReviewOmdbByS = restTemplate.getForEntity(omdbUrlByS, MovieReviewOmdbByS.class)
 					.getBody();
 			if (movieReviewOmdbByS.isResponse()) {
@@ -66,12 +65,8 @@ public class RestClient {
 				while (iter.hasNext()) {
 					Search search = iter.next();
 					MovieSummary summary = new MovieSummary();
-					//Async call to omdb api
-					CompletableFuture<MovieSummary> omdb = setMovieReviewsFromOmdb(search.getTitle(), summary);
-					//Async call to nyt api
-					CompletableFuture<MovieSummary> nyt = setSummaryShortFromNyt(search.getTitle(), summary);
-					//Wait till above 2 apis complete their job.
- 					CompletableFuture.allOf(omdb, nyt).join();			
+					setMovieReviewsFromOmdb(search.getTitle(), summary);
+					setSummaryShortFromNyt(search.getTitle(), summary);
 					summaryList.add(summary);
 				}
 				response.setMovieSummaryList(summaryList);
@@ -85,13 +80,10 @@ public class RestClient {
 		}
 		return response;
 	}
-	
-	@Async
-	private CompletableFuture<MovieSummary> setMovieReviewsFromOmdb(String title, MovieSummary summary) {
-		//logger.info("setMovieReviewsFromOmdb");
+
+	private MovieSummary setMovieReviewsFromOmdb(String title, MovieSummary summary) {
 		MovieReviewOmdbByT movieReviewByT = restTemplate
-				.getForEntity(omdbUrl + "/?t=" + title + "&apikey=" + omdbUrlApikey,
-						MovieReviewOmdbByT.class)
+				.getForEntity(omdbUrl + "/?t=" + title + "&apikey=" + omdbUrlApikey, MovieReviewOmdbByT.class)
 				.getBody();
 		summary.setTitle(movieReviewByT.getTitle());
 		summary.setPlot(movieReviewByT.getPlot());
@@ -100,17 +92,13 @@ public class RestClient {
 		summary.setImdbRating(movieReviewByT.getImdbRating());
 		summary.setImdbVotes(movieReviewByT.getImdbVotes());
 		summary.setLanguage(movieReviewByT.getLanguage());
-		return CompletableFuture.completedFuture(summary);
+		return summary;
 	}
-	
-	@Async
-	private CompletableFuture<MovieSummary> setSummaryShortFromNyt(String title, MovieSummary summary) {
-		//logger.info("setSummaryShortFromNyt");
+
+	private MovieSummary setSummaryShortFromNyt(String title, MovieSummary summary) {
 		MovieReviewNyt movieReviewNyt = restTemplate
-				.getForEntity(nytUrl + "api-key=" + nytUrlApikey + "&query=" + title,
-						MovieReviewNyt.class)
-				.getBody();
-		if(movieReviewNyt.getNum_results() == 0) {
+				.getForEntity(nytUrl + "api-key=" + nytUrlApikey + "&query=" + title, MovieReviewNyt.class).getBody();
+		if (movieReviewNyt.getNum_results() == 0) {
 			summary.setSummary_short("N/A");
 		}
 		Iterator<Results> iterNyt = movieReviewNyt.getResults().iterator();
@@ -120,6 +108,6 @@ public class RestClient {
 				summary.setSummary_short(result.getSummary_short());
 			}
 		}
-		return CompletableFuture.completedFuture(summary);
+		return summary;
 	}
 }
